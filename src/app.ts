16f2e0cca1a3520aef5c95828c070abfa83aad8e
Ismail Öz <Ismail z>
2016-04-03 12:24:25 +0200

    Bugfixes

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ export class App {
 
         config.title = 'Aurelia - Contacts';
         config.map([
-            { route: '',              name: '',             moduleId: 'no-selection',   title: 'Select'},
+            { route: '/',              name: '',             moduleId: 'no-selection',   title: 'Select'},
             { route: 'contacts',  name:'contacts',      moduleId: 'no-selection' },
             { route: 'contacts/:id',  name:'contacts',      moduleId: 'contact-detail' }
         ]);
